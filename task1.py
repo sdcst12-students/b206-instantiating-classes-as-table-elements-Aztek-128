05761fdfff3fb91ace27884163e7f5e681dd26f8
Aztek-128 <124203485+Aztek-128@users.noreply.github.com>
2023-10-10 12:34:21 -0700

    yes

--- task1.py
+++ task1.py
@@ -7,32 +7,51 @@ class team:
     games = 0
     results = {}
 
-    def points(self, points):
-        return
+    def points(self, points):        
+        recordData[self.name]["Points"] +=points
+        
     
-    def record(self, records):
-        return
+    #def record(self, records):
+        #if self.name 
+        #return
 
     def win(self, wins):
-        return
+        if wins["s1"] > wins["s2"]:
+            recordData[self.name]["Wins"] +=1
+        else:
+            pass
+        
     
     def loss(self, losses):
-        return
+        if losses["s1"] < losses["s2"]:
+            recordData[self.name]["Losses"] +=1
+        elif losses["s1"] == losses["s2"]:
+            recordData[self.name]["Ties"] +=1
+        
     
-    def __init__(self):
-        self.name = team
+    def __init__(self, teammate):
+        self.name = teammate
+        recordData[self.name]["GamesPlayed"]+=1
+
         
 Players = ["A","B","C","D","E","F","G"]
 gameData = [{'t1': 'A', 's1': 2, 't2': 'B', 's2': 3}, {'t1': 'A', 's1': 4, 't2': 'C', 's2': 3}, {'t1': 'A', 's1': 1, 't2': 'D', 's2': 0}, {'t1': 'A', 's1': 0, 't2': 'E', 's2': 2}, {'t1': 'A', 's1': 4, 't2': 'F', 's2': 2}, {'t1': 'A', 's1': 2, 't2': 'G', 's2': 4}, {'t1': 'B', 's1': 2, 't2': 'A', 's2': 1}, {'t1': 'B', 's1': 2, 't2': 'C', 's2': 1}, {'t1': 'B', 's1': 3, 't2': 'D', 's2': 2}, {'t1': 'B', 's1': 3, 't2': 'E', 's2': 0}, {'t1': 'B', 's1': 3, 't2': 'F', 's2': 1}, {'t1': 'B', 's1': 2, 't2': 'G', 's2': 0}, {'t1': 'C', 's1': 0, 't2': 'A', 's2': 2}, {'t1': 'C', 's1': 0, 't2': 'B', 's2': 0}, {'t1': 'C', 's1': 3, 't2': 'D', 's2': 4}, {'t1': 'C', 's1': 1, 't2': 'E', 's2': 1}, {'t1': 'C', 's1': 3, 't2': 'F', 's2': 4}, {'t1': 'C', 's1': 3, 't2': 'G', 's2': 1}, {'t1': 'D', 's1': 1, 't2': 'A', 's2': 3}, {'t1': 'D', 's1': 2, 't2': 'B', 's2': 0}, {'t1': 'D', 's1': 4, 't2': 'C', 's2': 4}, {'t1': 'D', 's1': 2, 't2': 'E', 's2': 2}, {'t1': 'D', 's1': 3, 't2': 'F', 's2': 3}, {'t1': 'D', 's1': 2, 't2': 'G', 's2': 1}, {'t1': 'E', 's1': 2, 't2': 'A', 's2': 0}, {'t1': 'E', 's1': 4, 't2': 'B', 's2': 2}, {'t1': 'E', 's1': 0, 't2': 'C', 's2': 2}, {'t1': 'E', 's1': 3, 't2': 'D', 's2': 1}, {'t1': 'E', 's1': 0, 't2': 'F', 's2': 4}, {'t1': 'E', 's1': 2, 't2': 'G', 's2': 1}, {'t1': 'F', 's1': 2, 't2': 'A', 's2': 1}, {'t1': 'F', 's1': 3, 't2': 'B', 's2': 3}, {'t1': 'F', 's1': 1, 't2': 'C', 's2': 4}, {'t1': 'F', 's1': 0, 't2': 'D', 's2': 1}, {'t1': 'F', 's1': 0, 't2': 'E', 's2': 1}, {'t1': 'F', 's1': 2, 't2': 'G', 's2': 2}, {'t1': 'G', 's1': 4, 't2': 'A', 's2': 2}, {'t1': 'G', 's1': 0, 't2': 'B', 's2': 0}, {'t1': 'G', 's1': 1, 't2': 'C', 's2': 2}, {'t1': 'G', 's1': 0, 't2': 'D', 's2': 3}, {'t1': 'G', 's1': 4, 't2': 'E', 's2': 3}, {'t1': 'G', 's1': 3, 't2': 'F', 's2': 2}, {'t1': 'G', 's1': 2, 't2': 'F', 's2': 3}, {'t1': 'G', 's1': 2, 't2': 'E', 's2': 2}, {'t1': 'G', 's1': 4, 't2': 'D', 's2': 2}, {'t1': 'G', 's1': 3, 't2': 'C', 's2': 3}, {'t1': 'G', 's1': 0, 't2': 'B', 's2': 0}, {'t1': 'G', 's1': 0, 't2': 'A', 's2': 0}, {'t1': 'F', 's1': 1, 't2': 'G', 's2': 4}, {'t1': 'F', 's1': 2, 't2': 'E', 's2': 3}, {'t1': 'F', 's1': 1, 't2': 'D', 's2': 3}, {'t1': 'F', 's1': 0, 't2': 'C', 's2': 1}, {'t1': 'F', 's1': 4, 't2': 'B', 's2': 2}, {'t1': 'F', 's1': 4, 't2': 'A', 's2': 4}, {'t1': 'E', 's1': 4, 't2': 'G', 's2': 1}, {'t1': 'E', 's1': 3, 't2': 'F', 's2': 0}, {'t1': 'E', 's1': 0, 't2': 'D', 's2': 0}, {'t1': 'E', 's1': 0, 't2': 'C', 's2': 4}, {'t1': 'E', 's1': 3, 't2': 'B', 's2': 3}, {'t1': 'E', 's1': 4, 't2': 'A', 's2': 0}, {'t1': 'D', 's1': 0, 't2': 'G', 's2': 2}, {'t1': 'D', 's1': 3, 't2': 'F', 's2': 2}, {'t1': 'D', 's1': 2, 't2': 'E', 's2': 1}, {'t1': 'D', 's1': 3, 't2': 'C', 's2': 3}, {'t1': 'D', 's1': 4, 't2': 'B', 's2': 1}, {'t1': 'D', 's1': 3, 't2': 'A', 's2': 0}, {'t1': 'C', 's1': 2, 't2': 'G', 's2': 1}, {'t1': 'C', 's1': 1, 't2': 'F', 's2': 3}, {'t1': 'C', 's1': 4, 't2': 'E', 's2': 4}, {'t1': 'C', 's1': 4, 't2': 'D', 's2': 3}, {'t1': 'C', 's1': 1, 't2': 'B', 's2': 1}, {'t1': 'C', 's1': 3, 't2': 'A', 's2': 1}, {'t1': 'B', 's1': 3, 't2': 'G', 's2': 3}, {'t1': 'B', 's1': 3, 't2': 'F', 's2': 2}, {'t1': 'B', 's1': 3, 't2': 'E', 's2': 2}, {'t1': 'B', 's1': 3, 't2': 'D', 's2': 1}, {'t1': 'B', 's1': 4, 't2': 'C', 's2': 2}, {'t1': 'B', 's1': 0, 't2': 'A', 's2': 1}, {'t1': 'A', 's1': 2, 't2': 'G', 's2': 4}, {'t1': 'A', 's1': 1, 't2': 'F', 's2': 0}, {'t1': 'A', 's1': 4, 't2': 'E', 's2': 3}, {'t1': 'A', 's1': 4, 't2': 'D', 's2': 2}, {'t1': 'A', 's1': 1, 't2': 'C', 's2': 2}, {'t1': 'A', 's1': 4, 't2': 'B', 's2': 0}]
 #Ants, Bears, Cougars, Doges, Elephants, Foxes, Giants
-recordData = {i : {"scores": 0,"GamesPlayed": 0,"Wins": 0,"Losses": 0} for i in team}
+recordData = {i : {"Points": 0,"GamesPlayed": 0,"Wins": 0,"Losses": 0,"Ties": 0} for i in Players}
 #recordData = {"A":{"scores": 0,"GamesPlayed": 0,"Wins": 0,"Losses": 0},"B":{"scores": 0,"GamesPlayed": 0,"Wins": 0,"Losses": 0},"C":{"scores": 0,"GamesPlayed": 0,"Wins": 0,"Losses": 0},"D":{"scores": 0,"GamesPlayed": 0,"Wins": 0,"Losses": 0},"E":{"scores": 0,"GamesPlayed": 0,"Wins": 0,"Losses": 0},"F":{"scores": 0,"GamesPlayed": 0,"Wins": 0,"Losses": 0},"G":{"scores": 0,"GamesPlayed": 0,"Wins": 0,"Losses": 0}}
 
 for i in gameData:
     y = team(i["t1"])
     y.points(i["s1"])
-    y.wins(i)
-    y.losses(i)
+    y.win(i)
+    y.loss(i)
+
+    y = team(i["t2"])
+    y.points(i["s2"])
+    y.win(i)
+    y.loss(i)
+
+print(recordData)    
 
 '''
 Ants
--- task3.py
+++ task3.py
@@ -1,5 +1,28 @@
+#Create a database to keep track of random monsters in a game Use the existing template as a guide and populate a list:
+
+#Rules:
+
+#each primary stat is generated as 3 random dice rolls (random 1-6) (strength, intelligance, piety, agility, stamina, charm)
+#distribution of level is 1: 40%, 2: 30%, 3: 20%, 4: 10%
+#each npc gets random 1-10 hp per level
+#each npc gets some random money
+#30% chance to have 0-6 gold
+#50% chance go have 3-12 silver
+#if they had no gold, they have a 75% chance to have 4-20 copper
+#each gold is worth 10 silver and each silver is worth 10 copper
+#Create 100 NPC's Generate a report that shows the distribution of NPC's by level Generatea a report that shows the 
+# mean and standard deviation for the following:
+
+#HP
+#Wealth (in copper. For example 2 gold, 3 silver and 4 copper has a wealth of 234)
+
+import random
+from task2 import mean 
+
+
 class NPC:
     stats = { 'str' : 0, 'int' : 0, 'pie' : 0, 'agi' : 0, 'stm' : 0, 'cha' : 0 }
+    #print(stats)
     level = 0
     hp = 0
     gold = 0
@@ -7,5 +30,91 @@ class NPC:
     copper = 0
 
     def __init__(self):
-        return
+        self.stats =  { 'str' : 0, 'int' : 0, 'pie' : 0, 'agi' : 0, 'stm' : 0, 'cha' : 0 }
+        self.levelup()
+        pass
+    def traits(self):
+        attributes = ("strength","intelligence","piety","agility","stamina","charm")        
+        for e in self.stats:
+            for i in range(3):
+                self.stats[e] += random.randint(1,6)
+        return self.stats
+    
+    def levelup(self):
+        leveldistribution = ['1'] *40 + ['2'] * 30 + ['3'] * 20 + ['4'] * 10
+        levelselected = random.choice(leveldistribution)
+        self.levelhealth = int(levelselected)
+        return levelselected
+    
+    def raisedhp(self):
+        addedhp = 0
+        for i in range(self.levelhealth):
+            healthy = random.randint(1,10)
+            addedhp += healthy
+        return addedhp
+
+    def wealth(self):
+        chance = random.randint(1,10)
+        if chance <= 3:
+            gold = random.randint(0,6)
+        else:
+            gold = 0
+        chance = random.randint(1,10)
+        if chance <= 5:
+            silver = random.randint(3,12)
+        else:
+            silver = 0
+        if gold == 0:
+            chance = random.randint(1,100)
+            if chance <= 75:
+                copper = random.randint(4,20)
+            else:
+                copper = 0
+        else:
+            copper = 0
+        self.g = gold
+        self.s = silver
+        self.c = copper 
+        return ((gold* 100) + (silver *10) + copper)
+hplist=[]
+wlist = []
+for i in range(100):
+    y = NPC()
+    stats = y.traits()
+    #print(stats)
+    level = y.levelup()
+    #print(level)
+    hp = y.raisedhp()
+    #print(hp)
+    hplist.append(hp)
+    mobtotalwealth = y.wealth()
+    wlist.append(mobtotalwealth)
+    print(mobtotalwealth)
+    y = None
+    
+sumtotalwealth = sum(wlist)
+totalhealth = sum(hplist)
+
+meanhealth = (totalhealth/100)
+
+healthdeviant = [(i - meanhealth)**2 for i in hplist]
+hpvariant = sum(healthdeviant)/100
+hpvariable = (hpvariant**0.5)
+
+print(f"the mean health is: {meanhealth}, and the deviant is: {round(hpvariable,2)}")
+
+meantotalwealth = (sumtotalwealth/100)
+
+wealthdeviant = [(i - meantotalwealth)**2 for i in wlist]
+wvariant = sum(wealthdeviant)/100
+wvariable = (wvariant ** 0.5)
+
+print(f"the mean wealth is: {meantotalwealth}, and the deviant is: {round(wvariable,2)}")
+
+
+
+
+    
+
+
     
